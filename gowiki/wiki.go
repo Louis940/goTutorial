160635c1c158a26355e4a1427ba00edb154b9e66
Louis <louisross211@hotmail.com>
2024-04-21 18:35:10 +0100

    part2 & 3

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -1,7 +1,11 @@
+//go:build ignore
+
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"log"
+	"net/http"
 	"os"
 )
 
@@ -24,9 +28,33 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+func renderTemplate(w http.Request, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sampple page.")}
-	p1.save()
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save", saveHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
